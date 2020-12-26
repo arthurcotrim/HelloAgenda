c18c0b9d9554ce885b8e81f3594b54556210a62d
Arthur Cotrim Santos <arthur.cs147@gmail.com>
2020-12-26 14:31:25 -0300

    Change the functions and put it to work, creating two functions that insert and remove the data.

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,44 @@
+// GET THE ELEMENTS 
 const btn = document.querySelector('.btn');
-const table = document.querySelector('.table');
 const input = document.querySelector('.userInput');
+const table = document.querySelector('.table');
 
-btn.onclick = function () {
-    let numberRows = table.rows.length
-    let newRow = table.insertRow(numberRows)
-    let inputText = input.value;
+////////////////////////////////////////
 
-    if (input.value == '') {
-        alert("You didn't put anything");
+// ADD TASKS ON THE TABLE
+let addTask = () => {
+    let inputText = input.value; //GET INPUT'S VALUE
+
+    if (inputText == '') {
+        alert(`You didn't write anything`);
+        input.focus();
     }
     else {
-        for (var i = 0; i < 1; i++) {
-            var newCell = newRow.insertCell(0);
-            var newCellDone = newRow.insertCell();
-            var newCellRemoving = newRow.insertCell();
+        let numberRows = table.rows.length // GET THE AMOUNT OF ROWS
+        let newRow = table.insertRow(numberRows);
+        // CREATE A ROW UNDER THE LAST ONE, THAT'S WHY WE PASS THE ARGUMENT 
 
-            newCell.innerHTML = inputText;
-            newCellRemoving.innerHTML = '<a href="#" class="status-Remove" onclick=removeTask()>Remove</a>';
-            newCellDone.innerHTML = '<a href="#" class="status-Done" onclick=doneTask()>Done</a>';
-        }
+        var newCell = newRow.insertCell(0); // CREATE A CELL IN INITIAL POSITION
+        var newCellRemoving = newRow.insertCell(); // CREATE A CELL IN THE FOLLOWING POSITION
+
+        newCell.innerHTML = inputText; // PUT THE CELL'S VALUE (USER PASSED TO US)
+        newCellRemoving.innerHTML = '<a href="#" class="removeBtn status-Remove"  onclick="removeTask(this)">Remove</a>'; // PUT A LINK AND INVOLK A FUNCTION
+
+        input.value = '';
+        input.focus();
     }
+}
+
+// REMOVE TASKS
+let removeTask = (newRow) => {
+    var i = newRow.parentNode.parentNode.rowIndex;
+    table.deleteRow(i); // GO TO TABLE AND DELETE AN ESPECIFI ROW 
+
     input.value = '';
     input.focus();
 }
 
-function removeTask() {
+////////////////////////////////////////
 
-}
-
-function doneTask(){
-    
-}
+// WHEN CLICK ON BUTTON CALL THE VARIABLE addTask 
+btn.onclick = addTask;
